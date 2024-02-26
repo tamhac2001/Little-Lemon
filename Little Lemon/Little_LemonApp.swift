547f6ea8566440b8e554135ade7e2aa0467daae8
tamhac2001 <tamhac2001@gmail.com>
2024-02-26 15:19:31 +0700

    Initial Commit

---
+++ Little Lemon/Little_LemonApp.swift
@@ -0,0 +1,32 @@
+//
+//  Little_LemonApp.swift
+//  Little Lemon
+//
+//  Created by Tam Hac on 26/02/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Little_LemonApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
